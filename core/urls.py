c1ec546518187d37108c0079d8e9934063ffa74c
jahir98 <ajchica@est.itsgg.edu.ec>
2020-10-23 20:59:57 -0500

    initial commit

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('sample/', views.sample, name='sample'),
+    path('store/', views.store, name='store'),
+]
